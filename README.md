6bffd0d58d190f663a0d0673bf0a4fabe70879ab
aeryaguzov <dev.aeryaguzov@gmail.com>
2012-10-14 19:05:17 +0400

    initial migration

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 php-mysqlnd-examples
 ====================
 
-This examples helps you to understand the difference between old mysql driver in PHP and mysqlnd
+`mysqlnd` is a drop-in replacement for the MySQL Client Library (`libmysql`) for PHP
+
+This examples helps you to understand the difference between between `libmysql` and `mysqlnd`
+
+Installation
+------------
+
+In order to use this examples you must create `config.php` (use `config.php.example` as layout).
+Also you need to run `initial_migration.php`. This script creates table and inserts there test data
---
+++ initial_migration.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Initial db migration
+ *
+ * @author Andrey Ryaguzov <dev.aeryaguzov@gmail.com>
+ * @license MIT License (distributed with source code)
+ * @copyright (c) 2012 Andrey Ryaguzov <dev.aeryaguzov@gmail.com>
+ * @link https://github.com/aeryaguzov/php-mysqlnd-examples for the canonical source repository
+ */
+$cnf = include 'config.php';
+$link = new mysqli(
+    $cnf['db']['host'],
+    $cnf['db']['username'],
+    $cnf['db']['password'],
+    $cnf['db']['database'],
+    $cnf['db']['port']
+);
+
+$result = $link->query('DROP TABLE IF EXISTS `async_test`;');
+if ($result) {
+    $result = $link->query('
+        CREATE TABLE `async_test` (
+          `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+          `val` varchar(255) DEFAULT NULL,
+          PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    ');
+}
+
+if (!$result) {
+    echo 'Failed create table. Exit.' . PHP_EOL; die();
+}
+
+$bulk_count = $cnf['initial_migration']['bulk_count'];
+$rows_count = $cnf['initial_migration']['rows_count'];
+
+$count = 0;
+$query = '';
+$insert = 'INSERT INTO `async_test` (`id`, `val`) VALUES ';
+
+for ($i = 1; $i <= $rows_count; $i++) {
+    if ($count == $bulk_count) {
+        // run bulk insert
+        $query = substr($query, 0, strlen($query) - 2);
+        $query .= ';';
+        if ($link->query($query)) {
+            // reset counter
+            $count = 0;
+            $query = '';
+            echo 'Successfully inserted ' . $bulk_count . ' rows.' . PHP_EOL;
+        } else {
+            echo 'Failed insert ' . $bulk_count . ' rows. Exit.' . PHP_EOL; die();
+        }
+
+    }
+
+    if($count == 0) {
+        $query .= $insert;
+    }
+
+    if ($count < $bulk_count) {
+        $query .= '( "", "test' . $i . '" ), ';
+        $count++;
+    }
+}
